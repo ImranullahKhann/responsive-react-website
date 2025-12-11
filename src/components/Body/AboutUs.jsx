f42cdfbdbbc160f820e0486919363985acd36298
ImranullahKhann <imranullahkhann2004@gmail.com>
2025-12-11 22:07:34 +0500

    About Us and Services Completed

--- src/components/Body/AboutUs.jsx
+++ src/components/Body/AboutUs.jsx
@@ -1,4 +1,5 @@
 import Hero from "../Common/Hero.jsx"
+import InfoTile from "../Common/InfoTile.jsx"
 
 export default function AboutUs () {
     return (
@@ -6,7 +7,12 @@ export default function AboutUs () {
         // - Mission	
         // - Vision
         <>
-            <Hero posterLink="https://images.pexels.com/photos/35111683/pexels-photo-35111683.jpeg?_gl=1*t9wnsl*_ga*NDc1NjUzNzgwLjE3NTc4NjI4NDA.*_ga_8JE65Q40S6*czE3NjU0MDI1MTUkbzE1JGcxJHQxNzY1NDAyNzM0JGo1OSRsMCRoMA.." title="About" /> 
+            <Hero posterLink="https://images.pexels.com/photos/35111683/pexels-photo-35111683.jpeg?_gl=1*t9wnsl*_ga*NDc1NjUzNzgwLjE3NTc4NjI4NDA.*_ga_8JE65Q40S6*czE3NjU0MDI1MTUkbzE1JGcxJHQxNzY1NDAyNzM0JGo1OSRsMCRoMA.." title="About" />
+            <div className="about-sections flex flex-col items-center gap-5 my-10">
+                <InfoTile title={"Our History"}/>
+                <InfoTile title={"Our Mission"} />
+                <InfoTile title={"Our Vision"} />
+            </div>
         </>
     )
 }
--- src/components/Body/Services.jsx
+++ src/components/Body/Services.jsx
@@ -1,10 +1,11 @@
 import Hero from "../Common/Hero.jsx"
+import RecentTrips from "../Common//RecentTrips/RecentTrips.jsx"
 
 export default function Services () {
     return (
-        // Recent Trips
         <>
             <Hero posterLink="https://images.unsplash.com/photo-1506012787146-f92b2d7d6d96?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" title="Services" /> 
+            <RecentTrips />
         </>
     )
 }
