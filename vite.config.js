ffe945acc4429bcddba07b0a9392ad0a6a13927d
Imranullah Khan <imranullahkhann2004@gmail.com>
2026-04-13 22:05:56 +0500

    Update Vite config to use environment variables

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all variables regardless of prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  base: "/",
+    base: env.BASE_URL,
+  }
 })
